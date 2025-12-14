f3d900b4058203068afcf556e6699ad8bd1fdc6e
DaoChaShao <27882272@qq.com>
2025-12-15 00:13:47 +0800

    feat: add cfg_types.py for configuration management of language, tasks, and sequence models

---
+++ src/configs/cfg_types.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2025/12/3 23:20
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   cfg_types.py
+# @Desc     :   
+
+from enum import StrEnum, unique
+from pathlib import Path
+
+from src.configs.cfg_base import CONFIG
+from src.utils.stats import load_json
+
+
+@unique
+class Lang(StrEnum):
+    CN = "cn"
+    EN = "en"
+
+
+@unique
+class SeqTask(StrEnum):
+    SEQ2ONE = "seq2one"
+    SEQ2SEQ = "seq2seq"
+    SEQ_SLICE = "slice"
+
+
+@unique
+class Seq2SeqNet(StrEnum):
+    RNN = "rnn"
+    LSTM = "lstm"
+    GRU = "gru"
+    TRANSFORMER = "transformer"
+
+
+@unique
+class Seq2SeqStrategies(StrEnum):
+    GREEDY = "greedy"
+    BEAM_SEARCH = "beam"
+
+
+@unique
+class Tokens(StrEnum):
+    PAD = "<PAD>"
+    UNK = "<UNK>"
+    SOS = "<SOS>"  # Or, call it BOS (Beginning of Sequence)
+    EOS = "<EOS>"
+    MASK = "<MASK>"
+    BOS = "<BOS>"  # Beginning of Sequence
+
+
+@unique
+class Tasks(StrEnum):
+    CLASSIFICATION = "classification"
+    GENERATION = "generation"
+
+
+if __name__ == "__main__":
+    out = SeqTask.SEQ2ONE
+    print(out)
+
+    dic: Path = Path(CONFIG.FILEPATHS.DICTIONARY)
+    dictionary: dict = load_json(dic)
+    print(dictionary[Tokens.PAD])

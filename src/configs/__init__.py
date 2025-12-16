6ecb6cbc7685efbbd0ff765533fb53f9840662bb
DaoChaShao <27882272@qq.com>
2025-12-16 15:35:54 +0800

    fix: update configuration categories and version in __init__.py

--- src/configs/__init__.py
+++ src/configs/__init__.py
@@ -15,13 +15,13 @@ modules for machine learning, NLP, CV, and general data processing.
 
 Main Categories:
 + cfg_base       : Basic file paths and general config (CONFIG, FilePaths)
-+ cfg_base4dl    : Deep learning base parameters (CONFIG4DL, DataPreprocessor, Hyperparameters)
++ cfg_dl         : Deep learning base parameters (CONFIG4DL, DataPreprocessor, Hyperparameters)
 + cfg_cnn        : CNN-specific parameters (CONFIG4CNN, CNNParams)
-+ cfg_mlp        : MLP/NLP-specific parameters (CONFIG4MLP, NLPParams)
++ cfg_mlp        : MLP-specific parameters (CONFIG4MLP, MLPParams)
 + cfg_rnn        : RNN-specific parameters (CONFIG4RNN, RNNParams)
 + cfg_unet       : UNet-specific parameters (CONFIG4UNET, UNetParams)
-+ eMun: Some necessary options functionality
-+ parser         : Parser module
++ cfg_types      : Type definitions and enums (Attentions, Langs, Tasks, Tokens, etc.)
++ parser         : Parser module for command line arguments
 
 Usage:
 + Access default configuration: e.g., CONFIG4CNN.CNN_PARAMS.OUT_CHANNELS
@@ -32,7 +32,7 @@ Usage:
 """
 
 __author__ = "Shawn Yu"
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 from .cfg_base import CONFIG, Config, Database, FilePaths, Punctuations
 from .cfg_cnn import CONFIG4CNN, Configuration4CNN, CNNParams
@@ -40,7 +40,7 @@ from .cfg_dl import CONFIG4DL, Config4DL, DataPreprocessor, Hyperparameters
 from .cfg_mlp import CONFIG4MLP, Configuration4MLP, MLPParams
 from .cfg_rnn import CONFIG4RNN, Configuration4RNN, RNNParams
 from .cfg_unet import CONFIG4UNET, Configuration4UNet, UNetParams
-from .cfg_types import Langs, TSSeqSeparate, Seq2SeqNets, Seq2SeqStrategies, Tokens, Tasks
+from .cfg_types import Attentions, Langs, Seq2SeqNets, Seq2SeqStrategies, Tasks, Tokens, TSSeqSeparate
 from .parser import set_argument_parser
 
 __all__ = [
@@ -50,6 +50,6 @@ __all__ = [
     "CONFIG4MLP", "Configuration4MLP", "MLPParams",
     "CONFIG4RNN", "Configuration4RNN", "RNNParams",
     "CONFIG4UNET", "Configuration4UNet", "UNetParams",
-    "Langs", "TSSeqSeparate", "Seq2SeqNets", "Seq2SeqStrategies", "Tokens", "Tasks",
+    "Attentions", "Langs", "Seq2SeqNets", "Seq2SeqStrategies", "Tasks", "Tokens", "TSSeqSeparate",
     "set_argument_parser"
 ]

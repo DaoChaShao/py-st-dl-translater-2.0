6687c66965fda9eb0aee60e3598f041a0b41d3a7
DaoChaShao <27882272@qq.com>
2025-12-18 16:42:11 +0800

    fix: enhance type hints in multi_task_gru.py to support Literal types for accelerator and task

--- src/nets/multi_task_gru.py
+++ src/nets/multi_task_gru.py
@@ -9,7 +9,7 @@
 from torch import (Tensor, nn,
                    cat, zeros, arange,
                    randint)
-from typing import override
+from typing import override, Literal
 
 from src.configs.cfg_types import Tasks
 from src.nets.base_rnn import BaseRNN
@@ -19,10 +19,10 @@ class MultiTaskGRU(BaseRNN):
     def __init__(self,
                  vocab_size: int, embedding_dim: int, hidden_size: int, num_layers: int,
                  dropout_rate: float = 0.3, bidirectional: bool = True,
-                 accelerator: str = "cpu",
+                 accelerator: str | Literal["cuda", "cpu"] = "cpu",
                  PAD: int = 0,
                  *,
-                 task: Tasks = Tasks.CLASSIFICATION, class_num: int = 2,
+                 task: str | Tasks | Literal["classification", "generation"] = "classification", class_num: int = 2,
                  ) -> None:
         super().__init__(
             vocab_size=vocab_size,
